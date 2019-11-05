82af457f8331df86826eeae557830f2e97f6ad12
Greg Leitheiser <greg@servantscode.org>
2019-11-05 15:35:09 -0600

    Fixed registration portal

--- src/app/dummy-dialog/dummy-dialog.component.ts
+++ src/app/dummy-dialog/dummy-dialog.component.ts
@@ -8,7 +8,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 })
 export class DummyDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<PhotoUploadDialogComponent>,
+  constructor(public dialogRef: MatDialogRef<DummyDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit() {
